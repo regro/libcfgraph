85dbc42d297fc38a2e6375c6970cf17e64a7117f
Matthew R. Becker <beckermr@users.noreply.github.com>
2023-04-05 21:41:37 -0500

    Create generate_file_listing.py

---
+++ generate_file_listing.py
@@ -0,0 +1,27 @@
+import json
+import glob
+import hashlib
+
+N_FILES = 10
+
+
+def _fname_to_index(fname):
+    return (
+        abs(int(hashlib.sha1(fname.encode("utf-8")).hexdigest(), 16))
+        % N_FILES
+    )
+
+
+all_fnames = glob.glob('artifacts/**/*.json', recursive=True)
+all_finds = [_fname_to_index(fname) for fname in all_fnames]
+
+for i in range(N_FILES):
+    with open(".file_listing_%d.json" % i, "w") as fp:
+        json.dump(
+            [fname for fname, find in zip(all_fnames, all_finds) if find == i],
+            fp,
+            indent=2,
+        )
+
+with open(".file_listing_meta.json", "w") as fp:
+    json.dump({"n_files": N_FILES}, fp, indent=2)
